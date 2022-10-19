e18167b2480857183e65b98618e17e202ccafaba
Francisco-OsoLop <francisco.osoriolop@gmail.com>
2022-10-19 11:56:32 -0500

    Agregando algo de JS

---
+++ js/index.js
@@ -0,0 +1,10 @@
+var nombre = "Francisco";
+var altura = 172;
+
+document.write(nombre);
+document.write(altura);
+datos.innerHTML = `
+<h1>Caja de datos</h1>
+<h2>Mi nombre es: ${nombre}</h2>
+<h3>Mido: ${altura} cm</h3>
+`;
